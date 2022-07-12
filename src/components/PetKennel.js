691dcf3d0dd72f3fae78ff55fa327f4baf2b9baf
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-07-12 16:17:54 -0500

    added employee components

--- src/components/PetKennel.js
+++ src/components/PetKennel.js
@@ -1,6 +1,7 @@
 //import React from "react"
 import { AnimalCard } from "./animals/AnimalCard";
 import { Location } from "./locations/Location";
+import { Employee } from "./employees/Employee";
 import "./PetKennel.css"
 
 export const PetKennel = () => (
@@ -17,6 +18,13 @@ export const PetKennel = () => (
             <AnimalCard />
             <AnimalCard />
             <AnimalCard />
+        </article>
+
+        <h2>Employees</h2>
+        <article className="employees">
+            <Employee />
+            <Employee />
+            <Employee />
         </article>
 
         <h2>Locations</h2>
