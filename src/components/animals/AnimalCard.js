9462bf74aca4ec9b48f3a1dcf5db457085748d98
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-07-20 14:52:14 -0500

    created animal detail component with dynamic routing incorporated

--- src/components/animals/AnimalCard.js
+++ src/components/animals/AnimalCard.js
@@ -1,16 +1,15 @@
+import React from "react"
 import "./Animal.css"
+import { Link } from "react-router-dom"
 
 export const AnimalCard = ({ animal }) => (
-    // props: { animal, customer, location }
-    <>
-        <section className="animal">
-            <h3 className="animal__name">{animal.name}</h3>
-            <div className="animal__species">Species: {animal.species}</div>
-            <div className="animal__breed">Breed: {animal.breed}</div>
-            <address className="animal__location">{animal.location.name}</address>
-            <div className="animal__owner">Customer: {animal.customer.name}</div>
-            {/* <div className="animal__location">Location: {location.name}</div>
-            <div className="animal__owner">Customer: {customer.name}</div> */}
-        </section>
-    </>
+    <section className="animal">
+        <h3 className="animal__name">
+          <Link to={`/animals/detail/${animal.id}`}>
+            { animal.name }
+          </Link>
+        </h3>
+        <div className="animal__species">{ animal.species}</div>
+        <div className="animal__breed">{ animal.breed }</div>
+    </section>
 );
